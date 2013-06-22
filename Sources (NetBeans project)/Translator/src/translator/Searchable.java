8befd17b2fb399f7b41ef281cbe6cff4f9f1690a
Ivan Zaycev <zaycev.ivan@gmail.com>
2013-06-23 01:52:40 +0600

    load

---
+++ Sources (NetBeans project)/Translator/src/translator/Searchable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package translator;
+
+/**
+ *
+ * @author ZaytsevID
+ */
+public interface Searchable {
+    public void search(String str);
+}
